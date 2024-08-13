f99ef83ba7fb6f30678dd786626a73d190dfe94b
Laurie Hufford <lauriehufford@gmail.com>
2024-08-13 12:25:55 +0200

    Add repo model, and update repo-details screen to show async icon at top

---
+++ GitExplorer/Models.swift
@@ -0,0 +1,26 @@
+//
+//  Models.swift
+//  GitExplorer
+//
+//  Created by Laurie Hufford on 13/08/2024.
+//
+
+import Foundation
+
+struct Repository: Equatable {
+    var id: String
+    var name: String
+    var iconURL: URL?
+    var orgName: String
+    var description: String
+    var language: String
+    var forkCount: Int
+    var openIssuesCount: Int
+    var stargazersCount: Int
+    var lastReleaseVersion: String
+}
+
+extension Repository {
+    static let dummy_a = Repository(id: "dummy-a", name: "strapi", iconURL: URL(string: "https://avatars.githubusercontent.com/u/19872173?s=200&v=4"), orgName: "strapi", description: "🚀 Strapi is the leading open-source headless CMS. It’s 100% JavaScript/TypeScript, fully customizable and developer-first.", language: "JavaScript", forkCount: 203, openIssuesCount: 266, stargazersCount: 30127, lastReleaseVersion: "3.1.6")
+    static let dummy_b = Repository(id: "dummy-b", name: "strapdown", iconURL: nil, orgName: "arturadib", description: "Instant and elegant Markdown documents in the browser", language: "JavaScript", forkCount: 12, openIssuesCount: 0, stargazersCount: 127, lastReleaseVersion: "4.2")
+}
--- GitExplorer/ShowRepoView.swift
+++ GitExplorer/ShowRepoView.swift
@@ -8,11 +8,54 @@
 import SwiftUI
 
 struct ShowRepoView: View {
+    
+    let repo: Repository
+    
+    func missingRepoIconView() -> some View {
+        RoundedRectangle(cornerRadius: 6)
+            .fill(Color(red: 233/255, green: 250/255, blue: 250/255))
+            .overlay(content: {
+                Image("missing-repo-icon")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .padding(28)
+            })
+    }
+    
     var body: some View {
-        Text("Show Repo")
+        VStack(alignment: .center, spacing: 0) {
+            AsyncImage(
+                url: repo.iconURL,
+                transaction: .init(animation: .default)
+            ) { phase in
+                if let image = phase.image {
+                    image
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                } else if phase.error != nil {
+                    missingRepoIconView()
+                } else {
+                    Color(red: 233/255, green: 250/255, blue: 250/255)
+                }
+            }
+            .cornerRadius(6)
+            .frame(width: 100, height: 100)
+            .padding(.bottom, 14)
+             
+            Text("\(repo.name) / \(repo.orgName)")
+                .font(.system(size: 16, weight: .medium))
+                .foregroundColor(Color("PrimaryText"))
+                .padding(.bottom, 6)
+            Text("\(repo.language)")
+                .font(.system(size: 14, weight: .regular))
+                .foregroundColor(Color("SecondaryText"))
+                .padding(.bottom, 6)
+            
+            Spacer()
+        }
     }
 }
 
 #Preview {
-    ShowRepoView()
+    ShowRepoView(repo: .dummy_a)
 }
