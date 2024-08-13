f1b83102d146e52476e91a7720003a3b5649d793
Laurie Hufford <lauriehufford@gmail.com>
2024-08-13 11:14:28 +0200

    Initial xcode project

---
+++ GitExplorer/GitExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitExplorerApp.swift
+//  GitExplorer
+//
+//  Created by Laurie Hufford on 13/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct GitExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
